b857c92c89edee4d1065cf258054393566fa07fb
Şamil Akpınar <samilakpinar6@gmail.com>
2025-05-31 15:47:00 +0300

    BCS -> 3105 -> gitignore eklendi ve düzenleme yapıldı

--- BankingCreditSystem.WebApi/Program.cs
+++ BankingCreditSystem.WebApi/Program.cs
@@ -18,7 +18,3 @@ app.UseCustomExceptionMiddleware();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
